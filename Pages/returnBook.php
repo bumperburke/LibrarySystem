bd8b9b6d72698b1d14e23968da8e49bc9731a21e
bumperburke <c12723809@mydit.ie>
2015-04-21 17:49:37 +0100

    Bug Fix: returnBook.php

--- Pages/returnBook.php
+++ Pages/returnBook.php
@@ -7,18 +7,30 @@
 	
 	
 	$isbnMatched = mysqli_query($mysqli, "SELECT isbn FROM books WHERE ISBN='{$isbn}'");
-	$matched = mysqli_fetch_array($isbnMatched);
+	$Imatched = mysqli_fetch_array($isbnMatched);
 	
-	if($matched["isbn"] == $isbn)
+	$userMatched = mysqli_query($mysqli, "SELECT username FROM users WHERE username='{$username}'");
+	$Umatched = mysqli_fetch_array($userMatched);
+	
+	if($isbn == "" || $username == "")
+	{
+		header("refresh:2; url=home.php");
+		echo '<h3 style="color:red">No ISBN/Username Entered, Please Try Again.</h3>';
+	}
+	
+	else if($Imatched["isbn"] == $isbn && $Umatched["username"] == $username)
 	{	
-		$ammendHistory = mysqli_query($mysqli, "UPDATE history SET status='Returned', returned='Y' WHERE isbn='{$isbn}' AND username='{username}'");
+		$ammendHistory = mysqli_query($mysqli, "UPDATE history SET status='Returned', returned='Y' WHERE isbn='{$isbn}' AND username='{$username}'");
 		$incrementQuantity = mysqli_query($mysqli, "UPDATE books SET quantity=quantity+1 WHERE ISBN='{$isbn}'");
 		header("refresh:0; url=home.php");
 	}
 	
-	else
+	else if($Imatched["isbn"] != $isbn || $Umatched["username"] != $username )
 	{
 		header("refresh:2; url=home.php");
-		echo '<h3 style="color:red">Invalid ISBN, Please Try Again.</h3>';
+		echo '<h3 style="color:red">Invalid ISBN/Username, Please Try Again.</h3>';
 	}
+	
+	
+	
 ?>
